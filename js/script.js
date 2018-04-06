fb54b47ec92bf3e1136d97b482ae96dd13316cbd
jens87alpen <jenshoffmann20@gmail.com>
2018-04-06 10:54:23 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,183 @@
+
+  // niceScroll
+  $("html").niceScroll();
+    
+    
+  // Stick menu
+  $(".menu").sticky({topSpacing:0});
+
+//hide show-info-link on page load
+$('.show-info-link').hide();
+  //hide-show map
+  $('.show-map').on('click',function(){
+    $('.contact-info').hide();
+    $('.show-info-link').show();
+
+  })
+
+  $('.show-info-link').on('click', function(){
+    $('.show-info-link').hide();
+    $('.contact-info').show();
+
+  })
+
+
+
+
+
+  // Menu Scroll to content and Active menu
+  var lastId,
+    topMenu = $("#menu"),
+    topMenuHeight = topMenu.outerHeight()+145,
+    menuItems = topMenu.find("a"),
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+   $('a[href*=#]').bind('click', function(e) {
+	e.preventDefault();
+       
+	var target = $(this).attr("href");
+			
+
+	$('html, body').stop().animate({ scrollTop: $(target).offset().top-140 }, 1000, function() {
+
+	});
+			
+	return false;
+   });
+
+  $(window).scroll(function(){
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+  });  
+  
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    
+    $(".footer").css( "position", "relative" );
+    $(".contact").css( "marginBottom", "0" );
+
+}
+else 
+{
+
+  // FadeTo elements
+  if ( $(window).width() > 1023) {  
+
+    tiles = $("h2, h3, .column-one, .column-two, .column-three, .grid li, .contact .content .form, .contact .content .contact-text ").fadeTo(0, 0);
+
+    $(window).scroll(function(d,h) {
+      tiles.each(function(i) {
+          a = $(this).offset().top + $(this).height();
+          b = $(window).scrollTop() + $(window).height();
+          if (a < b) $(this).fadeTo(1000,1);
+      });
+    });
+
+  }
+
+}
+
+
+
+  //Menu mobile click
+  $( ".icon" ).click(function() {
+    $( " ul.menu-click" ).slideToggle( "slow", function() {
+    // Animation complete.
+    });
+  });
+
+
+$(window).load(function(){
+
+$(".preloader").delay(500).fadeOut("slow")
+
+  // Parallax
+  if ($('.parallax-background').length) {
+    $(".parallax-background").parallax();
+  }
+  
+  // Parallax
+  if ($('.parallax-background-partners').length) {
+    // $(".parallax-background-partners").parallax();
+  }  
+
+});
+//jQuery is required to run this code
+$( document ).ready(function() {
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+});
+
+function scaleVideoContainer() {
+
+    var height = $(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+
+}
+
+function initBannerVideoSize(element){
+
+    $(element).each(function(){
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element){
+
+    var windowWidth = $(window).width(),
+    windowHeight = $(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+    // console.log(windowHeight);
+
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if(windowWidth < 1000){
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+            $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+    });
+}
